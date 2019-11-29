f13f4e1cc31d4bb1e4c02ce2ed7870e4c2413dad
yubin.li <liyubin.724@163.com>
2019-11-29 13:44:01 +0800

    添加Excel转Lua配置工具

---
+++ ExcelToData/ExcelToData/Config/ConfigExporter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+
+namespace Dot.Core.Config
+{
+    public class SimpleLuaExporter
+    {
+        private StringBuilder luaSB = new StringBuilder();
+        public string Export(ConfigSheetData sheetData)
+        {
+            luaSB.Clear();
+            if(sheetData!=null)
+            {
+                luaSB.AppendLine($"local {sheetData.name} = {{");
+                ConfigFieldData firstFieldData = sheetData.fields[0];
+                foreach(var line in sheetData.lines)
+                {
+                    luaSB.AppendLine($"    [{line.contents[0].GetContent(firstFieldData)}] = {{");
+
+                    for(int i =0;i<line.contents.Length;i++)
+                    {
+                        ConfigFieldData fieldData = sheetData.fields[i];
+                        string content = line.contents[i].GetContent(fieldData);
+                        if(content == null || fieldData.DefineType == ConfigFieldType.None)
+                        {
+                            continue;
+                        }
+                        luaSB.Append($"        {fieldData.DefineName} = ");
+                        if(fieldData.DefineType == ConfigFieldType.Int || fieldData.DefineType == ConfigFieldType.Float)
+                        {
+                            luaSB.AppendLine($"{content},");
+                        }else if( fieldData.DefineType == ConfigFieldType.String)
+                        {
+                            luaSB.AppendLine($"\"{content}\",");
+                        }else if(fieldData.DefineType == ConfigFieldType.Stringt)
+                        {
+                            luaSB.AppendLine($"[[{content}]],");
+                        }else if(fieldData.DefineType == ConfigFieldType.Lua)
+                        {
+                            luaSB.AppendLine($"function()\n{content}\n        end,");
+                        }else if(fieldData.DefineType == ConfigFieldType.Array)
+                        {
+                            luaSB.AppendLine("{");
+                            string[] values = content.Split(new string[] { "[", "]", "##" }, StringSplitOptions.RemoveEmptyEntries);
+                            if(values!=null && values.Length>0)
+                            {
+                                foreach(var v in values)
+                                {
+                                    if(fieldData.InnerFieldType == ConfigFieldType.Float || fieldData.InnerFieldType == ConfigFieldType.Int)
+                                    {
+                                        luaSB.AppendLine($"            {v},");
+                                    }else if(fieldData.InnerFieldType == ConfigFieldType.String)
+                                    {
+                                        luaSB.AppendLine($"            \"{v}\",");
+                                    }else if(fieldData.InnerFieldType == ConfigFieldType.Stringt)
+                                    {
+                                        luaSB.AppendLine($"            [[{content}]],");
+                                    }
+                                }
+                            }
+                            luaSB.AppendLine("        },");
+                        }else if(fieldData.DefineType == ConfigFieldType.Dic)
+                        {
+                            luaSB.AppendLine("{");
+                            string[] contents = content.Substring(1,content.Length-1).Split(new string[] { "##"}, StringSplitOptions.RemoveEmptyEntries);
+
+                            luaSB.AppendLine("        },");
+                        }
+                    }
+
+                    luaSB.AppendLine("    },");
+                }
+                luaSB.AppendLine("}");
+                luaSB.AppendLine($"return {sheetData.name}");
+            }
+
+
+            return luaSB.ToString();
+        }
+    }
+}
---
+++ ExcelToData/ExcelToData/Program.cs
@@ -0,0 +1,70 @@
+﻿using Dot.Core.Config;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Game.Core.ExcelToData
+{
+    class Program
+    {
+        static void ExportConfig(string excelPath)
+        {
+            ConfigWorkbookData configWorkbook = ExcelWorkbook.ParseWorkbook(excelPath);
+
+            if (configWorkbook != null && configWorkbook.sheets != null)
+            {
+                foreach (ConfigSheetData sheet in configWorkbook.sheets)
+                {
+                    StringBuilder errorMsg = new StringBuilder();
+                    foreach (var line in sheet.lines)
+                    {
+                        for (int i = 0; i < line.contents.Length; i++)
+                        {
+                            ConfigFieldData fieldData = sheet.fields[i];
+                            string error = "";
+                            if (fieldData.VerifyCompose != null && !fieldData.VerifyCompose.Verify(sheet, line.contents[i], out error))
+                            {
+                                errorMsg.AppendLine(error);
+                            }
+                        }
+                    }
+                    if (errorMsg.Length > 0)
+                    {
+                        errorMsg.Insert(0, "---------" + sheet.name);
+                        DebugLogger.Error(errorMsg.ToString());
+                    }
+                    else
+                    {
+                        SimpleLuaExporter exporter = new SimpleLuaExporter();
+                        string value = exporter.Export(sheet);
+                        if(!Directory.Exists("./exporter_lua"))
+                        {
+                            Directory.CreateDirectory("./exporter_lua");
+                        }
+
+                        File.WriteAllText(@"./exporter_lua/" + sheet.name + ".txt", value, new UTF8Encoding(false));
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] xlsFiles = Directory.GetFiles("./", "*.xls", SearchOption.AllDirectories);
+            string[] xlsxFiles = Directory.GetFiles("./", "*.xlsx", SearchOption.AllDirectories);
+
+            foreach(var f in xlsFiles)
+            {
+                ExportConfig(f);
+            }
+            foreach(var f in xlsxFiles)
+            {
+                ExportConfig(f);
+            }
+
+            
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+        }
+    }
+}
