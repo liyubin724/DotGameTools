acb5f81898e438da3a9a327e9487eac02d6bea03
li.yubin <liyubin724@163.com>
2019-12-10 00:52:53 +0800

    i

---
+++ DotExcelToData/DotExcelToData/Datas/FieldType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot.Tools.ETD.Datas
+{
+    public enum FieldType
+    {
+        None = 0,
+
+        Int,
+        Ref,
+        Long,
+        Float,
+        Bool,
+        String,
+        Stringt,
+        Res,
+        Arr,
+        Dic,
+
+        Max,
+    }
+}
