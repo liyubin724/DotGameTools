9058ba206706990f4bef6060b64e5a777176ea42
yubin.li <liyubin.724@163.com>
2019-12-12 15:04:24 +0800

    添加NUnit测试内容

---
+++ DotExcelToData/DotExcelToDataTest/Factorys/ValidationFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Dot.Tools.ETD.Factorys;
+using Dot.Tools.ETD.Validations;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotExcelToDataTest.Factorys
+{
+    [TestFixture]
+    public class ValidationFactoryTest
+    {
+        [Test]
+        public void TestGetValidation()
+        {
+            string validationStr = "IntValue";
+            IValidation validation = ValidationFactory.GetValidation(validationStr);
+            Assert.AreEqual(typeof(IntValueValidation), validation.GetType(), $"ValidationFactoryTest::TestGetValidation->{validationStr}");
+
+            validationStr = "IntRange{10,100}";
+            validation = ValidationFactory.GetValidation(validationStr);
+            Assert.AreEqual(typeof(IntRangeValidation), validation.GetType(), $"ValidationFactoryTest::TestGetValidation->{validationStr}");
+            Assert.AreEqual(100, ((IntRangeValidation)validation).max, $"ValidationFactoryTest::TestGetValidation->{validationStr}");
+        }
+        [Test]
+        public void TestGetValidations()
+        {
+
+        }
+    }
+}
---
+++ DotExcelToData/DotExcelToDataTest/Utils/ContentUtilTest.cs
@@ -0,0 +1,37 @@
+﻿using Dot.Tools.ETD.Utils;
+using NUnit.Framework;
+
+namespace DotExcelToDataTest.Utils
+{
+    [TestFixture]
+    public class ContentUtilTest
+    {
+        [Test]
+        public void TestSplitContent()
+        {
+            string str = "a;b;c;";
+            string[] strArr = ContentUtil.SplitContent(str, new char[] { ';' });
+            Assert.IsNotNull(strArr, "ContentUtilTest::TestSplitContent->Array is null.str = " + str);
+            Assert.AreEqual(3, strArr.Length);
+
+            str = "a;b\\;c;";
+            strArr = ContentUtil.SplitContent(str, new char[] { ';' });
+            Assert.IsNotNull(strArr, "ContentUtilTest::TestSplitContent->Array is null.str = " + str);
+            Assert.AreEqual(2, strArr.Length);
+            Assert.AreEqual(@"b;c", strArr[1]);
+
+
+            str = "a,1;b,2;c;3";
+            strArr = ContentUtil.SplitContent(str, new char[] { ';' ,','});
+            Assert.IsNotNull(strArr, "ContentUtilTest::TestSplitContent->Array is null.str = " + str);
+            Assert.AreEqual(6, strArr.Length);
+            Assert.AreEqual("a", strArr[0]);
+
+            str = @"a,test\,11;b,2;c;3";
+            strArr = ContentUtil.SplitContent(str, new char[] { ';', ',' });
+            Assert.IsNotNull(strArr, "ContentUtilTest::TestSplitContent->Array is null.str = " + str);
+            Assert.AreEqual(6, strArr.Length);
+            Assert.AreEqual(@"test,11", strArr[1]);
+        }
+    }
+}
