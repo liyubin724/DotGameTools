a15cd1c14522a708b0a54c9770e617b7d423aca2
li.yubin <liyubin724@163.com>
2019-12-20 02:08:21 +0800

    添加Unity使用Log4net

---
+++ DotLog/DotLog/FilterRollingFileAppender.cs
@@ -0,0 +1,14 @@
+﻿using log4net.Appender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot.Log
+{
+    public class FilterRollingFileAppender : RollingFileAppender
+    {
+
+    }
+}
---
+++ DotLog/DotLog/LogUtil.cs
@@ -0,0 +1,12 @@
+﻿using log4net;
+
+namespace Dot.Log
+{
+    public static class LogUtil
+    {
+        public static ILog Logger(string tag)
+        {
+            return LogManager.GetLogger(tag);
+        }
+    }
+}
