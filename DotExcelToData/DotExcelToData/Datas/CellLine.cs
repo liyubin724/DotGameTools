b6e99e281c4ca8f586174ba91dc9d35d1a3d11b3
yubin.li <liyubin.724@163.com>
2020-01-15 17:48:52 +0800

    删除库引用

--- DotExcelToData/DotExcelToData/Datas/CellLine.cs
+++ DotExcelToData/DotExcelToData/Datas/CellLine.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Dot.Tools.ETD.Datas
 {
     public class CellLine
     {
         public int Row { get; set; }
-
         public List<CellContent> cells = new List<CellContent>();
-
-
-
     }
 }
